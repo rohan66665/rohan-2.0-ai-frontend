0f6446f2438647b90d933a1751d25df0824e0cfc
ROHAN SHARMA <rohan.sharma723@gmail.com>
2025-12-03 20:01:14 +0530

    removed old rag-frontend + using new vite frontend

--- rag-frontend/src/config.js
+++
@@ -1,7 +0,0 @@
-// src/config.js
-// Backend ka base URL yahan set karo
-// Local pe:
-export const API_BASE_URL = "http://127.0.0.1:8000";
-
-// Deploy ke baad yahan apna AWS backend URL daal dena:
-// export const API_BASE_URL = "https://YOUR-EC2-DOMAIN-OR-IP:8000";
